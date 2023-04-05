bb360ba289ab276d091ddc1a650d65b8f9101bc9
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2023-04-05 19:55:39 +1000

    ~Fixed bad union calculation

--- algorithm/jaccard.sql
+++ algorithm/jaccard.sql
@@ -1,38 +1,41 @@
-WITH target_links AS (
-  SELECT to_article_id
+WITH targets AS (
+  SELECT to_article_id as article_id
   FROM links
-  WHERE from_article_id = ? -- target ID
+  WHERE from_article_id = ?
 ),
-intersection_count AS (
-  SELECT l.from_article_id, COUNT(*) AS intersections
-  FROM links l
-  INNER JOIN target_links tl ON
-    l.to_article_id = tl.to_article_id
-  WHERE l.from_article_id != ? -- target ID
-  GROUP BY l.from_article_id
+target_count AS (
+	Select Count(*) as num
+	From targets
 ),
-union_count AS (
-  SELECT l.from_article_id, COUNT(*) AS unions
+intersections AS (
+	SELECT
+		links.from_article_id AS article_id,
+		COUNT(*) AS num
+	FROM links
+	WHERE links.from_article_id != ? and links.to_article_id in targets
+	GROUP BY links.from_article_id
+),
+unions AS (
+  SELECT l.from_article_id as article_id, COUNT(*) + tc.num AS num
   FROM (
     SELECT from_article_id, to_article_id
     FROM links
-    WHERE from_article_id != ? -- target ID
-    UNION
-    SELECT ? AS from_article_id, to_article_id -- target ID
-    FROM target_links
+    WHERE from_article_id != ?
+			and to_article_id not in targets -- remove double counts
   ) l
+	INNER JOIN target_count as tc on true
   GROUP BY l.from_article_id
 ),
 jaccard_similarity AS (
-  SELECT ic.from_article_id as id,
-    ic.intersections,
-    uc.unions,
-    (CAST(ic.intersections AS FLOAT) / uc.unions) AS similarity
-  FROM intersection_count ic
-  JOIN union_count uc ON ic.from_article_id = uc.from_article_id
+  SELECT ic.article_id as id,
+    ic.num as intersections,
+    uc.num as unions,
+    (CAST(ic.num AS FLOAT) / uc.num) AS similarity
+  FROM intersections ic
+  JOIN unions uc ON ic.article_id = uc.article_id
 )
 SELECT articles.title, js.similarity, (js.intersections || '/' || js.unions) as helper
 FROM jaccard_similarity js
 INNER JOIN articles ON js.id = articles.id
-ORDER BY js.similarity DESC
+ORDER BY js.similarity DESC, js.unions DESC
 LIMIT 1000;
