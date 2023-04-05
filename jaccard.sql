c695b4a3cb54c3eaa3ea10a74cf3286848c50cfa
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2023-04-05 17:13:26 +1000

    +More detailed interaction

--- jaccard.sql
+++ jaccard.sql
@@ -1,13 +1,13 @@
 WITH target_links AS (
   SELECT to_article_id
   FROM links
-  WHERE from_article_id = ? -- replace ? with the target ID
+  WHERE from_article_id = ? -- target ID
 ),
 intersection_count AS (
   SELECT l.from_article_id, COUNT(*) AS intersection
   FROM links l
   JOIN target_links tl ON l.to_article_id = tl.to_article_id
-  WHERE l.from_article_id != ? -- replace ? with the target ID
+  WHERE l.from_article_id != ? -- target ID
   GROUP BY l.from_article_id
 ),
 union_count AS (
@@ -15,21 +15,23 @@ union_count AS (
   FROM (
     SELECT from_article_id, to_article_id
     FROM links
-    WHERE from_article_id != ? -- replace ? with the target ID
+    WHERE from_article_id != ? -- target ID
     UNION
-    SELECT ? AS from_article_id, to_article_id -- replace ? with the target ID
+    SELECT ? AS from_article_id, to_article_id -- target ID
     FROM target_links
   ) l
   GROUP BY l.from_article_id
 ),
 jaccard_similarity AS (
   SELECT ic.from_article_id as id,
-         (CAST(ic.intersection AS FLOAT) / uc.union_count) AS similarity
+    ic.intersection,
+    uc.union_count,
+    (CAST(ic.intersection AS FLOAT) / uc.union_count) AS similarity
   FROM intersection_count ic
   JOIN union_count uc ON ic.from_article_id = uc.from_article_id
 )
-SELECT articles.title, jaccard_similarity.similarity
-FROM jaccard_similarity
-INNER JOIN articles ON jaccard_similarity.id = articles.id
-ORDER BY similarity DESC
-LIMIT 20;
+SELECT articles.title, js.intersection, js.union_count, js.similarity
+FROM jaccard_similarity js
+INNER JOIN articles ON js.id = articles.id
+ORDER BY js.similarity DESC
+LIMIT 1000;
--- search.py
+++ search.py
@@ -5,27 +5,62 @@ cursor = conn.cursor()
 
 
 jaccard_sql = ""
+results = None
+result_offset = 0
 
 # read in the SQL file
 with open('jaccard.sql', 'r') as f:
     jaccard_sql = f.read()
 
+def ShowSimilar(target):
+  cursor.execute("Select a.title from articles a where a.title like ? order by length(a.title) limit 5;", ( "%"+target+"%" ,))
+  results = cursor.fetchall()
+  print("  Try: " + ", ".join(str(val[0]) for val in results))
+
+  pass
+
 def search(target):
+  global results
+  global result_offset
+
   cursor.execute("SELECT id FROM articles WHERE title = ?", (target,))
   res = cursor.fetchone()
 
   if res is None:
-    print("Invalid")
+    print("No match")
+    ShowSimilar(target)
     return
 
   target_id = res[0]
 
   cursor.execute(jaccard_sql, (target_id, target_id, target_id, target_id,))
   results = cursor.fetchall()
+  result_offset = 0
 
-  formatted_results = "  " + "\n  ".join(" ".join(str(value) for value in row) for row in results)
+  ShowResults()
+
+def ShowResults():
+  global results
+  global result_offset
+
+  if results is None:
+    print('No search present')
+    return
+
+  formatted_results = "  " + "\n  ".join(f'{row[3]:.4f} {row[0]} {row[1]}/{row[2]}' for row in results[result_offset:result_offset+10])
+  result_offset = result_offset + 10
   print(formatted_results)
 
+def RunCommand(cmd):
+  match cmd:
+    case ".more":
+      ShowResults()
+    case _:
+      print(f'Unknown command {cmd}')
 
 while True:
-  search(input("Search: "))
+  cmd = input("> ")
+  if cmd[0] == ".":
+    RunCommand(cmd)
+  else:
+    search(cmd)
